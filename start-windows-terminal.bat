7fceeaa8200367e445ba994abd83d13023e8e4f0
Bjarte Aune Olsen <bjarte@basementmedia.no>
2020-07-24 20:56:41 +0200

    Batch file for opening Windows Terminal with custom settings (like multiple tabs and panes)

---
+++ start-windows-terminal.bat
@@ -0,0 +1,6 @@
+:: This is really just a single line command. Split up with ^ for readability
+start "" wt -p "Command Prompt"; ^
+new-tab -p "Ubuntu-20.04" -d C:\Projects\Project1; ^
+new-tab -p "Ubuntu-20.04" -d C:\Projects\Project2\Subdir1; ^
+split-pane -p "Ubuntu-20.04" -d C:\Projects\Project2\Subdir2; ^
+focus-tab -t 0
